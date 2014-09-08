7b1a3ecad87cfd46fd022e6acc030f109c6f5eea
Mitar <mitar.git@tnode.com>
2014-09-08 13:24:46 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,20 +1,10 @@
 Package.describe({
   summary: "Making async functions blocking",
   version: '0.4.2',
-  name: 'peerlibrary:blocking',
+  name: 'mrt:blocking',
   git: 'https://github.com/peerlibrary/meteor-blocking.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
-  api.export('blocking');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:blocking', 'tinytest', 'test-helpers'], 'server');
-  api.add_files('tests.js', 'server');
+  api.imply('peerlibrary:blocking@0.4.2');
 });
