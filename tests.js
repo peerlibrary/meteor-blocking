2a897e9c213a32c19fe038ab3daaa8db59109994
Mitar <mitar.git@tnode.com>
2015-01-16 20:44:51 -0800

    Added test.

--- tests.js
+++ tests.js
@@ -11,6 +11,8 @@ Tinytest.add('blocking', function (test) {
 
   test.equal(blocking(function (a, b, cb) { cb(null, a + b) })(1, 2), 3);
 
+  test.equal(blocking(function (a, b, f, cb) { cb(null, f(a, b)) })(1, 2, function (a, b) {return a + b}), 3);
+
   test.throws(function () {
     blocking(function (a, b, cb) { cb("test error") })(1, 2)
   }, /^test error$/);
