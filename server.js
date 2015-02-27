6f1946b125f58ff76ede9df6a25bb10bbabc04b2
Mitar <mitar.git@tnode.com>
2015-02-26 21:47:39 -0800

    Scope isolation is not needed in Meteor packages.

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-(function () {
-  var Future = Npm.require('fibers/future');
+var Future = Npm.require('fibers/future');
 
-  // Inside blocking context functions should not be throwing exceptions but
-  // call callback with first argument an error. Exceptions will not propagate
-  // and will only be printed to the console.
-  blocking = function (obj, fun) {
-    if (!fun) {
-      fun = obj;
-      obj = undefined;
+// Inside blocking context functions should not be throwing exceptions but
+// call callback with first argument an error. Exceptions will not propagate
+// and will only be printed to the console.
+blocking = function (obj, fun) {
+  if (!fun) {
+    fun = obj;
+    obj = undefined;
+  }
+  var future = new Future();
+  var f = function () {
+    if (_.isUndefined(obj)) {
+      obj = this;
     }
-    var future = new Future();
-    var f = function () {
-      if (_.isUndefined(obj)) {
-        obj = this;
-      }
-      var args = _.toArray(arguments);
-      fun.apply(obj, args.concat(future.resolver()));
-      return future.wait();
-    };
-    f._blocking = true;
-    return f;
+    var args = _.toArray(arguments);
+    fun.apply(obj, args.concat(future.resolver()));
+    return future.wait();
   };
-})();
+  f._blocking = true;
+  return f;
+};
